e7134e9c7a1b1789b20237c58d9c6a9721964036
Aakash Prajapati <aakashap309@gmail.com>
2025-01-05 17:55:07 +0530

    social logins

--- resources/views/livewire/login.blade.php
+++ resources/views/livewire/login.blade.php
@@ -1,4 +1,4 @@
-<div class="max-w-sm mx-auto bg-white p-8 rounded-lg shadow-lg mt-10">
+<div class="max-w-xl mx-auto bg-white p-8 rounded-lg shadow-lg mt-10">
     @if($isLogin)
     <h2 class="text-2xl font-semibold mb-6 ">Sign Up</h2>
     <!-- Form -->
@@ -25,8 +25,24 @@
             <button type="submit" class="bg-blue-500 text-white px-6 py-2 rounded hover:bg-blue-600 focus:outline-none">Sign Up</button>
         </div>
     </form>
-    <hr class="m-3">
-    <p>Already have account ? <button class="underline text-blue-500" wire:click="switchLogin">Log In</button></p>
+    <hr class="my-6">
+    <p class="text-center">Sign up with</p>
+    <div class="space-y-3 mt-4">
+        <button class="w-full border border-blue-600 text-blue-600 px-4 py-2 rounded-md hover:bg-blue-600 hover:text-white transition focus:outline-none">
+            Facebook
+        </button>
+        <button class="w-full border border-gray-800 text-gray-800 px-4 py-2 rounded-md hover:bg-gray-800 hover:text-white transition focus:outline-none">
+            GitHub
+        </button>
+        <button class="w-full border border-red-500 text-red-500 px-4 py-2 rounded-md hover:bg-red-500 hover:text-white transition focus:outline-none">
+            Google
+        </button>
+        <button class="w-full border border-blue-400 text-blue-400 px-4 py-2 rounded-md hover:bg-blue-400 hover:text-white transition focus:outline-none">
+            Twitter
+        </button>
+    </div>
+    <hr class="my-6">
+    <p>Already have an account? <button class="underline text-blue-500" wire:click="switchLogin">Log In</button></p>
     @else
 
     <h2 class="text-2xl font-semibold mb-6">Log In</h2>
@@ -49,7 +65,24 @@
             <button type="submit" class="bg-blue-500 text-white px-6 py-2 rounded hover:bg-blue-600 focus:outline-none">Log In</button>
         </div>
     </form>
-    <hr class="m-3">
-    <p>Don't have account ? <button class="underline text-blue-500" wire:click="switchLogin ">Sign Up</button></p>
+    <hr class="my-2">
+    <p class="text-center">Log in with</p>
+    <div class="space-y-3 mt-4">
+        <button class="w-full border border-blue-600 text-blue-600 px-4 py-2 rounded-md hover:bg-blue-600 hover:text-white transition focus:outline-none">
+            Facebook
+        </button>
+        <button class="w-full border border-gray-800 text-gray-800 px-4 py-2 rounded-md hover:bg-gray-800 hover:text-white transition focus:outline-none">
+            GitHub
+        </button>
+        <button class="w-full border border-red-500 text-red-500 px-4 py-2 rounded-md hover:bg-red-500 hover:text-white transition focus:outline-none">
+            Google
+        </button>
+        <button class="w-full border border-blue-400 text-blue-400 px-4 py-2 rounded-md hover:bg-blue-400 hover:text-white transition focus:outline-none">
+            Twitter
+        </button>
+    </div>
+
+    <hr class="my-6">
+    <p>Don't have an account? <button class="underline text-blue-500" wire:click="switchLogin">Sign Up</button></p>
     @endif
 </div>
