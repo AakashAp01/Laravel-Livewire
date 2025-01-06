e9f2c3ca565b5b30ea2bec992b66ba5f926023b1
Aakash Prajapati <aakashap309@gmail.com>
2025-01-06 21:50:58 +0530

    update cart

--- resources/views/livewire/cart-button.blade.php
+++ resources/views/livewire/cart-button.blade.php
@@ -1,8 +1,8 @@
 <a href="/cart" wire:navigate class="hover:bg-gray-700 px-3  rounded-md text-lg flex items-center">
     Cart
     @if($cartCount > 0)
-    <span class="bg-red-500 text-white text-sm font-bold rounded-full ml-2 px-2 py-1">
-        {{ $cartCount }}
-    </span>
+        <span class="bg-red-500 text-white text-sm font-bold rounded-full ml-2 px-2 py-1">
+            {{ $cartCount }}
+        </span>
     @endif
 </a>
